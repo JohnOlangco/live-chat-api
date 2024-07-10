205cfcd1722a452cf6d9ff1d08f715b807673323
John <>
2024-07-10 21:19:56 +0800

    WIP

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,41 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/login', function() {
+    return ;
+})->name('login');
+
+// Group Routing
+// Route::group(['prefix' => 'admin'], function() {
+//     Route::get('/dashboard', function() {
+//         return 'Admin Dashboard';
+//     });
+
+//     Route::get('/users', function() {
+//         return 'Admin Users';
+//     });
+// });
+
+// Group Routing with Middleware
+Route::group(['middleware' => 'auth'], function() {
+    Route::get('/profile', function() {
+        return 'User Profile';
+    });
+
+    Route::get('/settings', function() {
+        return 'User Settings';
+    });
+});
+
+// Combining Prefix and Middleware
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
+    Route::get('dashboard', function() {
+        return 'Admin Dashboard';
+    });
+
+    Route::get('/users', function() {
+        return 'Admin Users';
+    });
+});
